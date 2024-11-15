0496592fa0419f3aa20a12df3ea501bc984e30a5
T-ctrl <cesarduran302@gmail.com>
2024-11-15 17:15:38 -0600

    sesiones anonimas

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -12,10 +12,20 @@ def index(request):
     num_authors = Author.objects.count()
     num_genre = Genre.objects.count()
 
-    return render(request, 'index.html', 
-                  context={'num_books':num_books, 'num_instances':num_instances, 
-                           'num_instances_available':num_instances_available, 
-                           'num_authors':num_authors, 'num_genre':num_genre})
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
+    context = {
+        'num_books':num_books, 
+        'num_instances':num_instances, 
+        'num_instances_available':num_instances_available, 
+        'num_authors':num_authors, 
+        'num_genre':num_genre,
+        'num_visits':num_visits,
+    }
+
+    return render(request, 'index.html', context=context)
 
 class BookListView(generic.ListView):
     model = Book
