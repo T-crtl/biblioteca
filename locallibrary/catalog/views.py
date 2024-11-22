f3218a40eb87972187030d04ae402471708e52d7
T-ctrl <cesarduran302@gmail.com>
2024-11-22 17:02:46 -0600

    update forms basic CRUD

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -106,15 +106,15 @@ def renew_book_librarian(request, pk):
 
     return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
 
-class AuthorCreate(CreateView):
+class AuthorCreate(LoginRequiredMixin, CreateView):
     model = Author
     fields = '__all__'
     initial={'date_of_death':'05/01/2018',}
 
-class AuthorUpdate(UpdateView):
+class AuthorUpdate(LoginRequiredMixin, UpdateView):
     model = Author
     fields = ['first_name','last_name','date_of_birth','date_of_death']
 
-class AuthorDelete(DeleteView):
+class AuthorDelete(LoginRequiredMixin, DeleteView):
     model = Author
     success_url = reverse_lazy('authors')
